b2666a10fe332aecf29cbb49ce00ab0553f853c7
Alexandr Kacheryants <kacheryants30@gmail.com>
2022-02-27 00:20:00 +0300

    Change search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,27 +1,59 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonRouter, useIonViewDidLeave, useIonViewWillEnter } from '@ionic/react';
 import { searchOutline } from 'ionicons/icons';
 import { useEffect, useState } from 'react';
-import { searchAnimes } from './../apis/Shikimori';
+import { useParams } from 'react-router';
+import { FilmData, search } from '../apis/Kinopoisk';
+import { FilmItem } from '../components/List';
+import { Animes, searchAnimes } from './../apis/Shikimori';
 import './Page.css';
 
 const SearchPage: React.FC = () => {
   const [searchString, setSearchString] = useState<string>("");
+  const [filmSearchResult, setFilmSearchResult] = useState<FilmData[] | null>(null);
+  const [animeSearchResult, setAnimeSearchResult] = useState<Animes[] | null>(null);
+  const router = useIonRouter();
+  const { query } = useParams<{ query: string; }>();
 
-  useEffect(()=>{
-    searchAnimes(searchString);
-  }, [searchString])
+  useEffect(() => {
+    if (searchString.length > 3){
+      (async ()=>{
+        var animeRes = await searchAnimes(searchString);
+        var filmRes = await search(searchString);
+        setFilmSearchResult(filmRes || null);
+      })();
+    }
+  }, [searchString, query])
 
-  useIonViewWillEnter(()=>{
-    
+  function searchData(query:string = searchString) {
+    (async ()=>{
+      var animeRes = await searchAnimes(query);
+      var filmRes = await search(query);
+      setFilmSearchResult(filmRes || null);
+      setAnimeSearchResult(animeRes || null);
+    })();
+  }
+  
+  useIonViewWillEnter(() => {
+
   })
+
+  useIonViewDidLeave(() => {
+    setSearchString("");
+    setFilmSearchResult(null);
+  })
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          <div slot='start'>
+            <IonBackButton />
+          </div>
           <IonInput onIonChange={(event) => {
-            setSearchString(event.detail.value?.toString() || "");
-          }} type='text' placeholder="Введите название фильма или сериала" />
-          <IonButton slot='end' fill='clear'>
+            var q = event.detail.value?.toString()
+            setSearchString(q || "");
+          }} value={searchString.length === 0 ? new URLSearchParams(window.location.search).get("q") || query || "" : searchString} type='text' placeholder="Введите название фильма или сериала" />
+          <IonButton slot='end' fill='clear' onClick={()=>{ searchData(new URLSearchParams(window.location.search).get("q") || query || "") }}>
             <IonIcon icon={searchOutline} />
           </IonButton>
         </IonToolbar>
@@ -34,7 +66,13 @@ const SearchPage: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonList>
-            
+          {
+            filmSearchResult ? filmSearchResult.map((film, idx)=>{
+              return (
+                <FilmItem data={film} />
+              );
+            }) : <IonItem>Ничего не найдено, или меньше 5 букв в строке поиска</IonItem>
+          }
         </IonList>
       </IonContent>
     </IonPage >
