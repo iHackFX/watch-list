50d25d0058bf008903a0e40abc7da5b6ae22365f
Alexandr Kacheryants <kacheryants30@gmail.com>
2022-02-27 00:17:53 +0300

    Refactor Kinopoisk API and add new features

--- src/apis/Kinopoisk.tsx
+++ src/apis/Kinopoisk.tsx
@@ -1,18 +1,21 @@
 import axios, { AxiosRequestConfig } from "axios";
 
+const headers = {
+    'X-API-KEY': process.env.REACT_APP_KINOPOISK_API_KEY || "",
+    'accept': 'application/json',
+}
+
 async function getTopFilms() {
     var config: AxiosRequestConfig<any> = {
         method: "get",
-        headers: {
-            'X-API-KEY': process.env.REACT_APP_KINOPOISK_API_KEY || "",
-            'accept': 'application/json',
-        },
+        headers: headers,
     }
     var data = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/top", config)
     return data.data.films as Array<FilmData>;
 }
 
 async function getTopSerials(page: number = 1) {
+    if (page < 0) return null;
     var config: AxiosRequestConfig<any> = {
         method: "get",
         params: {
@@ -24,14 +27,52 @@ async function getTopSerials(page: number = 1) {
             yearTo: 3000,
             page: page
         },
-        headers: {
-            'X-API-KEY': process.env.REACT_APP_KINOPOISK_API_KEY || "",
-            'accept': 'application/json',
-        },
+        headers: headers,
     }
     var data = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.2/films", config)
     console.log(data)
-    return data.data.items as Array<FilmData>;
+    return data?.data?.items as FilmData[] || null;
+}
+
+async function getFilm(id:number):Promise<null | undefined | GetFilmData> {
+    if (id == null) return null;
+    if (id <= 0) return null;
+    var config: AxiosRequestConfig<any> = {
+        method: "get",
+        headers: headers,
+    }
+
+    var data = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/" + id, config)
+    return data?.data as GetFilmData || null;
+}
+
+async function getTrailer(id:number):Promise<null | undefined | GetTrailerData[]> {
+    if (id == null) return null;
+    if (id <= 0) return null;
+    var config: AxiosRequestConfig<any> = {
+        method: "get",
+        headers: headers,
+    }
+
+    var data = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/" + id + "/videos", config)
+    return data?.data?.items as GetTrailerData[] || null;
+}
+
+async function search(q:string):Promise<null | undefined | FilmData[]> {
+    if (q == null) return null;
+    if (q.length < 1) return null;
+    var config: AxiosRequestConfig<any> = {
+        method: "get",
+        params:{
+            "keyword": q,
+            "page": 1
+        },
+        headers: headers,
+    }
+
+    var data = await axios.get("https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword", config)
+    console.log(data);
+    return data.data.films as FilmData[] || null;
 }
 
 interface FilmData {
@@ -61,6 +102,64 @@ interface FilmData {
     ratingChange?: any
 }
 
+interface GetFilmData{
+    kinopoiskId: number,
+    imdbId: string,
+    nameRu: null | string,
+    nameEn: null | string,
+    nameOriginal: string,
+    posterUrl: string,
+    posterUrlPreview: string,
+    reviewsCount: number | null,
+    ratingGoodReview: number | null,
+    ratingGoodReviewVoteCount: number | null,
+    ratingKinopoisk: number | null,
+    ratingKinopoiskVoteCount: number | null,
+    ratingImdb: number | null,
+    ratingImdbVoteCount: number | null,
+    ratingFilmCritics: number | null,
+    ratingFilmCriticsVoteCount: number | null,
+    ratingAwait: number | null,
+    ratingAwaitCount: number | null,
+    ratingRfCritics: number | null,
+    ratingRfCriticsVoteCount: number | null,
+    webUrl: string,
+    year: number,
+    filmLength: number,
+    slogan: string | null,
+    description: string | null,
+    shortDescription: string | null,
+    editorAnnotation: string | null,
+    isTicketsAvailable: Boolean,
+    productionStatus: string | null,
+    type: "FILM" | "TV_SHOW",
+    ratingMpaa: number | null,
+    ratingAgeLimits: number | null,
+    countries: [
+        {
+            country: string
+        }
+    ],
+    genres: [
+        {
+            genre: string
+        }
+    ],
+    startYear: string | null,
+    endYear: string | null,
+    serial: Boolean,
+    shortFilm: Boolean,
+    completed: Boolean,
+    hasImax: Boolean,
+    has3D: Boolean,
+    lastSync: string
+}
 
-export { getTopFilms, getTopSerials };
-export type { FilmData }
+interface GetTrailerData {
+    url: string,
+    name: string
+    site: "YOUTUBE" | "KINOPOISK_WIDGET" | "UNKNOWN"
+}
+
+export { getTopFilms, getTopSerials, getFilm, getTrailer, search };
+export type { FilmData, GetFilmData, GetTrailerData }
