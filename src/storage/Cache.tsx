027759fef9a7c458bec3305b02be7b527815e3dc
Alexandr Kacheryants <kacheryants30@gmail.com>
2022-03-27 20:14:50 +0300

    rework

--- src/storage/Cache.tsx
+++ src/storage/Cache.tsx
@@ -1,11 +1,12 @@
 import { FilmData, GetFilmData } from "../apis/Kinopoisk";
+import { GetAnimeData } from "../apis/Shikimori";
 import storage from "./StorageService";
 
 
 async function getCachedFilm(id: number){
     var data = await storage.get('kinopoisk') as GetFilmData[] || [] as GetFilmData[];
     var filtred = data.filter(function(item) { return item.kinopoiskId === id; });
-    return filtred[0] || null;
+    return filtred[0] || [] as GetFilmData[];
 }
 
 async function appendFilmCache(data: GetFilmData) {
@@ -15,7 +16,23 @@ async function appendFilmCache(data: GetFilmData) {
 }
 
 async function getCachedFilms() {
-    return await storage.get('kinopoisk');
+    return await storage.get('kinopoisk') as GetFilmData[] || [] as GetFilmData[];
 }
 
-export {getCachedFilm, getCachedFilms, appendFilmCache}
+async function getCachedAnime(id: number){
+    var data = await storage.get('shikimori') as GetAnimeData[] || [] as GetAnimeData[];
+    var filtred = data.filter(function(item) { return item.id === id; });
+    return filtred[0] || [] as GetAnimeData[];
+}
+
+async function appendAnimeCache(data: GetAnimeData) {
+    var arr = await storage.get('shikimori') as GetAnimeData[] || [] as GetAnimeData[];
+    arr.push(data);
+    await storage.set("shikimori", arr);
+}
+
+async function getCachedAnimes() {
+    return await storage.get('shikimori') as GetAnimeData[] || [] as GetAnimeData[];
+}
+
+export {getCachedFilm, getCachedFilms, appendFilmCache, getCachedAnime, appendAnimeCache, getCachedAnimes};
